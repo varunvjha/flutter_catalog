0f2424cf6722c807f2e166bd51c344465c1aa65d
Varun Jha <varun.jha@spense.money>
2023-08-27 16:04:50 +0530

    Added ListBuilder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   int days = 30;
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Center(child: Container(child: Text("Hello World"))),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(item: dummyList[index]);
+          },
+        ),
+      ),
       drawer: MyDrawer(),
     );
   }
